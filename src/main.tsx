352febb497dafa6df8297fb237f6dc1c146f5a74
GrayHead <ss.zhuravlov@gmail.com>
2025-03-18 18:20:04 +0200

    фів

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,48 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.tsx";
 import APage from "./pages/APage.tsx";
 import BPage from "./pages/BPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 
-const router = createBrowserRouter([
-    {
-        path: '/', element: <MainLayout/>,
-        children:
-            [
-                {path: '', element: <HomePage/>},
-                // /a
-                {path: 'a', element: <APage/>},
-                {
-                    path: 'b', element: <BPage/>, children: [
-                        // /b/1
-                        {path: '1', element: <div>content 1</div>},
-                        // /b/2
-                        {path: '2', element: <div>content 2</div>},
-                        // /b/3
-                        {path: '3', element: <div>content 3</div>},
-                    ]
-                },
-            ]
-
-    },
-
-
-])
+// const router = createBrowserRouter([
+//     {
+//         path: '/', element: <MainLayout/>,
+//         children:
+//             [
+//                 {path: '', element: <HomePage/>},
+//                 // /a
+//                 {path: 'a', element: <APage/>},
+//                 {
+//                     path: 'b', element: <BPage/>, children: [
+//                         // /b/1
+//                         {path: '1', element: <div>content 1</div>},
+//                         // /b/2
+//                         {path: '2', element: <div>content 2</div>},
+//                         // /b/3
+//                         {path: '3', element: <div>content 3</div>},
+//                     ]
+//                 },
+//             ]
+//     }
+// ])
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={router}/>);
+    .render(
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<MainLayout/>}>
+                    <Route path={''} element={<HomePage/>}/>
+                    <Route path={'a'} element={<APage/>}/>
+                    <Route path={'b'} element={<BPage/>}>
+                        <Route path={'1'} element={<div>content 1</div>}/>
+                        <Route path={'2'} element={<div>content 2</div>}/>
+                        <Route path={'3'} element={<div>content 3</div>}/>
+                    </Route>
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    );
