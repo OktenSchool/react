09a9a5e55a813efbc7bf2ab2244d311959b2a989
GrayHead <ss.zhuravlov@gmail.com>
2025-03-19 13:42:51 +0200

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,21 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router";
+import App from "./App.tsx";
 import {UsersPage} from "./pages/UsersPage.tsx";
 import {PostsPage} from "./pages/PostsPage.tsx";
 import {CommentsPage} from "./pages/CommentsPage.tsx";
 import {ProductsPage} from "./pages/ProductsPage.tsx";
-import App from "./App.tsx";
-
-const routes = createBrowserRouter(
-    [
-        {
-            path: '/', element: <App/>, children: [
-                {index: true, element: <div>hello default element</div>},
-                {path: 'users', element: <UsersPage/>},
-                {path: 'posts', element: <PostsPage/>},
-                {path: 'comments', element: <CommentsPage/>},
-                {path: 'products', element: <ProductsPage/>}
-            ]
-        },
-    ]
-);
-
-// /users
-// /posts
-// /comments
-// /products
 
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>);
+createRoot(document.getElementById('root')!).render(<BrowserRouter>
+    <Routes>
+        <Route path={'/'} element={<App/>}>
+            <Route path={'users'} element={<UsersPage/>}/>
+            <Route path={'posts'} element={<PostsPage/>}/>
+            <Route path={'comments'} element={<CommentsPage/>}/>
+            <Route path={'products'} element={<ProductsPage/>}/>
+        </Route>
+    </Routes>
+
+</BrowserRouter>);
