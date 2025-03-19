763b3988d87cbf6e88e3176845314ff44bb2a0bd
GrayHead <ss.zhuravlov@gmail.com>
2025-03-19 13:44:03 +0200

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,8 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
-import {UsersPage} from "./pages/UsersPage.tsx";
-import {PostsPage} from "./pages/PostsPage.tsx";
-import {CommentsPage} from "./pages/CommentsPage.tsx";
-import {ProductsPage} from "./pages/ProductsPage.tsx";
-import App from "./App.tsx";
-
-const routes = createBrowserRouter(
-    [
-        {
-            path: '/', element: <App/>, children: [
-                {index: true, element: <div>hello default element</div>},
-                {path: 'users', element: <UsersPage/>},
-                {path: 'posts', element: <PostsPage/>},
-                {path: 'comments', element: <CommentsPage/>},
-                {path: 'products', element: <ProductsPage/>}
-            ]
-        },
-    ]
-);
-
-// /users
-// /posts
-// /comments
-// /products
+import {RouterProvider} from "react-router";
+import {routes} from "./router.tsx";
 
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>);
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={routes}/>);
