a99c6bfb91c962f3ba1bf84f43b32be2b74022cc
GrayHead <ss.zhuravlov@gmail.com>
2025-03-26 17:26:45 +0200

    chszHaH9 part2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router";
-import {routes} from "./router/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+import {UsersComponent} from "./components/UsersComponent.tsx";
+import App from "./App.tsx";
 
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>);
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<App/>}>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<div>posts</div>}/>
+                <Route path={'comments'} element={<div>comments</div>}/>
+                <Route path={'products'} element={<div>products</div>}/>
+
+            </Route>
+
+        </Routes>
+
+    </BrowserRouter>);
